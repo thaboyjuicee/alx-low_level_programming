4fe180fc393a158d144b47b306aeb87bd989bad4
John Basil <62840750+Juicee999@users.noreply.github.com>
2022-11-03 00:35:33 +0100

    Codes for C-ing shege

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
+
+
+int _putchar(char c)
+{
+        printf("%c", c);
+        return(0);
+}
